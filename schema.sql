c82fcdf87b35a80ed435789db2039512b2f43646
Eric Mauldin <emauldin84@gmail.com>
2019-03-29 08:37:14 -0400

    creates songs table and populates with 5 examples

---
+++ schema.sql
@@ -0,0 +1,8 @@
+create table songs (
+    id serial primary key,
+    title varchar(100),
+    artist varchar(200),
+    album varchar(200),
+    year int
+
+)
---
+++ seed.sql
@@ -0,0 +1,7 @@
+insert into songs (title, artist, album, year)
+VALUES
+('The Moment', 'Tame Impala', 'Currents', 2015),
+('Gooey', 'Glass Animals', 'ZABA', 2014),
+('I Went To The Store One Day', 'Father Jonh Misty', 'I love you, Honeyber', 2015),
+('Honey Moon', 'Mac DeMarco', 'Honey Moon', 2018),
+('Shuggie', 'Foxygen', 'We Are the 21st Century Ambassadors of Peace & Magic', 2013);
